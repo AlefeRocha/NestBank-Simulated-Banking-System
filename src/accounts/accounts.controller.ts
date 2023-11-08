45d45a3e7981bfa728945477493654027346082e
Alefe Rocha <alefe.rtavares@gmail.com>
2023-11-08 11:01:06 -0300

    build(accounts file): Creating the account controller and service

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Delete, Get, Post } from "@nestjs/common";
+import { AccountsService } from "./accounts.service";
 
 @Controller('account')
 export class AccountsController {
-constructor() {}
+constructor(private accountsService: AccountsService) {}
 
     @Post()
     async create() {}
+
+    @Get()
+    async list() {}
+
+    @Get('/:id')
+    async getById() {}
+
+    @Delete('/:id')
+    async delete() {}
 }
