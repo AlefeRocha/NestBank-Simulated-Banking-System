30f6d5eb45f27d9b9e0f1a5c53e9c4e49c8d822d
Alefe Rocha <alefe.rtavares@gmail.com>
2023-11-08 17:53:15 -0300

    build(transaction): Finishing the controller, service and repositorie

--- src/transactions/transaction.controller.ts
+++ src/transactions/transaction.controller.ts
@@ -1,19 +1,27 @@
-import { Controller, Get, Put } from '@nestjs/common'
-import { TransactionsService } from './transaction.service';
+import { Body, Controller, Get, Param, Put } from '@nestjs/common'
+import { TransactionsService } from './transaction.service'
 
 @Controller('transaction')
 export class TransactionsController {
   constructor(private transactionService: TransactionsService) {}
 
   @Put('/:id/deposit')
-  async deposit() {}
+  async deposit(@Param('id') id: string, @Body() { value }: { value: number }) {
+    return await this.transactionService.deposit(id, value)
+  }
 
   @Put('/:id/withdraw')
-  async withdraw() {}
+  async withdraw(@Param('id') id: string, @Body() { value }: { value: number }) {
+    return await this.transactionService.withdraw(id, value)
+  }
 
   @Get('/:id/balance')
-  async balance() {}
+  async balance(@Param('id') id: string) {
+    return await this.transactionService.balance(id)
+  }
 
   @Get('/:id/transactions')
-  async transactionsList() {}
+  async transactionsList(@Param('id') id: string) {
+    return await this.transactionService.transactionsList(id)
+  }
 }
